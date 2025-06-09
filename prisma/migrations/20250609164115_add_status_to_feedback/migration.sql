ff2ddf456b189e2b9d88f4b7f4483df4ebd1e05f
Marcos Domingues <dfenixweb@gmail.com>
2025-06-09 13:47:55 -0300

    fix: remove feedbackId and rename createdAt to timestamp in Feedback model

---
+++ prisma/migrations/20250609164115_add_status_to_feedback/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `feedbackId` on the `Feedback` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Feedback" DROP COLUMN "feedbackId";
